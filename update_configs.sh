ed9f7b6014a81d4ad1199cf8950c7a4f11dd3039
freedan42x <danil123550@gmail.com>
2020-02-14 18:44:05 +0200

    folder sort

--- update_configs.sh
+++ update_configs.sh
@@ -1,6 +1,6 @@
-cp ~/.bashrc .
-cp ~/.emacs .
-cp /etc/nixos/configuration.nix .
+cp ~/.bashrc ./other
+cp ~/.emacs ./other
+cp /etc/nixos/configuration.nix ./nix
 git add .
 git commit -m "updated configs"
 git push
